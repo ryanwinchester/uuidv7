3914614ef2838b3e7550337342c2a6b0aa547b7a
Ryan Winchester <ryan@winchester.dev>
2024-03-30 21:09:31 -0300

    Add a couple tests

--- test/uuidv7_test.exs
+++ test/uuidv7_test.exs
@@ -10,4 +10,16 @@ defmodule UUIDv7Test do
   test "generates uuid binary" do
     assert <<_::128>> = UUIDv7.bingenerate()
   end
+
+  test "encode/1" do
+    assert uuid = UUIDv7.bingenerate()
+    assert encoded = UUIDv7.encode(uuid)
+    assert ^uuid = UUIDv7.decode(encoded)
+  end
+
+  test "decode/1" do
+    assert uuid = UUIDv7.generate()
+    assert decoded = UUIDv7.decode(uuid)
+    assert ^uuid = UUIDv7.encode(decoded)
+  end
 end
