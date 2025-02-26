dd210362d513138b2fcb280078c0ad64a076237b
Ryan Winchester <ryan@winchester.dev>
2025-02-26 09:33:33 -0400

    Update to match changes in Postgres implementation

--- lib/application.ex
+++ lib/application.ex
@@ -3,17 +3,13 @@ defmodule UUIDv7.Application do
   use Application
 
   def start(_type, _args) do
-    # Put the timestamp ref into persistent_term store.
-    # This is used in the `UUIDv7.Clock` module to determine the most recent
-    # timestamp, and protect against time leap backwards.
+    # Put the timestamp ref into `:persistent_term` store.
+    # This is used in the `UUIDv7.Clock` module to determine the most
+    # recently-used timestamp.
     clock_atomics_ref = :atomics.new(1, signed: false)
     :persistent_term.put(UUIDv7.Clock, clock_atomics_ref)
 
-    children = [
-      {UUIDv7.Clock, Application.get_env(:uuid_v7, UUIDv7.Clock, [])}
-    ]
-
     opts = [strategy: :one_for_one, name: UUIDv7.Supervisor]
-    Supervisor.start_link(children, opts)
+    Supervisor.start_link([], opts)
   end
 end
--- lib/clock.ex
+++ lib/clock.ex
@@ -1,128 +1,58 @@
 defmodule UUIDv7.Clock do
   @moduledoc false
-  use GenServer
 
-  @type timestamp :: integer()
-  @type counter :: <<_::18>>
-  @type counter_seed :: <<_::17>>
+  # For macOS (Darwin) or Windows use 10 bits.
+  # Otherwise, use 12 bits (e.g. on Linux).
+  step_bits =
+    case :os.type() do
+      {:unix, :darwin} -> 10
+      {:win32, _} -> 10
+      {_, _} -> 12
+    end
 
-  @default_table_opts [:named_table, :public, :set]
-  @default_cleanup_interval_ms :timer.seconds(2)
-  @default_cleanup_tick_cutoff 2
+  ns_per_ms = 1_000_000
 
-  # The threshold number where the counter will roll over.
-  @max_counter 2 ** 18
-
-  @compile {:inline, [update_counter: 2]}
+  @minimal_step_ns div(ns_per_ms, Bitwise.bsl(1, step_bits)) + 1
 
   @doc """
-  Get a unix millisecond timestamp and a clock sequence that fits in 18 bits.
+  Get an always-ascending unix nanosecond timestamp.
+
+  This is a re-implementation of the Postgres version here (in `C`):
+  https://github.com/postgres/postgres/blob/0e42d31b0b2273c376ce9de946b59d155fac589c/src/backend/utils/adt/uuid.c#L480
+
+  We use `:atomics` to ensure this works with concurrent executions without race
+  conditions.
   """
-  @spec next(counter_seed()) :: {timestamp(), counter()}
-  def next(<<seed::17>>) do
+  def next_ascending do
+    # Get the atomics ref that we set in `UUIDv7.Application.start/2`.
     timestamp_ref = :persistent_term.get(__MODULE__)
+
     previous_ts = :atomics.get(timestamp_ref, 1)
-    current_ts = System.system_time(:millisecond)
+    min_step_ts = previous_ts + @minimal_step_ns
+    current_ts = System.system_time(:nanosecond)
 
-    # Time leap backwards protection.
-    current_ts = if current_ts < previous_ts, do: previous_ts, else: current_ts
-
-    :atomics.put(timestamp_ref, 1, current_ts)
-
-    # Rollover protection.
-    # If the counter is over the allotted bits, then we update the timestamp
-    # by 1 millisecond to preserve order, which will also reset the counter
-    # using the provided seed value.
-    clock =
-      with @max_counter <- update_counter(current_ts, seed) do
-        next_ts = current_ts + 1
-        :atomics.put(timestamp_ref, 1, next_ts)
-        update_counter(next_ts, seed)
+    # If the current timestamp is not at least the minimal step nanoseconds
+    # greater than the previous step, then we make it so.
+    new_ts =
+      if current_ts > min_step_ts do
+        current_ts
+      else
+        min_step_ts
       end
 
-    {current_ts, <<clock::big-unsigned-18>>}
+    compare_exchange(timestamp_ref, previous_ts, new_ts)
   end
 
-  @doc """
-  Starts the Clock server.
+  defp compare_exchange(timestamp_ref, previous_ts, new_ts) do
+    case :atomics.compare_exchange(timestamp_ref, 1, previous_ts, new_ts) do
+      # If the new value was written, then we return it.
+      :ok ->
+        new_ts
 
-  The GenServer part of this module is used for managing the ETS table and not
-  for generating UUIDs.
-
-  ### Options
-
-   * `:cleanup_interval` - The interval in milliseconds that the table cleanup
-     task is run. Defaults to `#{@default_cleanup_interval_ms}`.
-   * `:cleanup_tick_cutoff` - The number of timestamp ticks ago that the table
-     is pruned to. Defaults to `#{@default_cleanup_tick_cutoff}`.
-
-  """
-  @spec start_link(keyword()) :: GenServer.on_start()
-  def start_link(opts) do
-    if Keyword.get(opts, :start?, true) do
-      name = Keyword.get(opts, :name) || __MODULE__
-      GenServer.start_link(__MODULE__, opts, name: name)
-    else
-      :ignore
+      # If the atomic value has changed in the meantime, we add the minimal step
+      # nanoseconds value to that and try again.
+      updated_ts ->
+        compare_exchange(timestamp_ref, updated_ts, updated_ts + @minimal_step_ns)
     end
-  end
-
-  # ----------------------------------------------------------------------------
-  # GenServer callbacks.
-  # ----------------------------------------------------------------------------
-
-  @impl GenServer
-  def init(opts) do
-    interval_ms = Keyword.get(opts, :cleanup_interval, @default_cleanup_interval_ms)
-    cleanup_tick_cutoff = Keyword.get(opts, :cleanup_tick_cutoff, @default_cleanup_tick_cutoff)
-    ets_opts = Keyword.get(opts, :table, @default_table_opts)
-
-    state = %{
-      table: :ets.new(__MODULE__, ets_opts),
-      interval_ms: interval_ms,
-      timestamp_ref: :persistent_term.get(__MODULE__),
-      cleanup_tick_cutoff: cleanup_tick_cutoff
-    }
-
-    schedule_cleanup(interval_ms)
-
-    {:ok, state}
-  end
-
-  @impl GenServer
-  def handle_info(:cleanup, state) do
-    cleanup(state.timestamp_ref, state.cleanup_tick_cutoff)
-    schedule_cleanup(state.interval_ms)
-    {:noreply, state}
-  end
-
-  # ----------------------------------------------------------------------------
-  # Manage ETS table.
-  # ----------------------------------------------------------------------------
-
-  # The current best solution for having a counter that resets for every
-  # millisecond tick is to use the millisecond timestamp as the keys for
-  # en `ets` table and use `update_counter`. It avoids having to use a
-  # GenServer for state, or have an ever-increasing monotonic integer that
-  # doesn't reset and introduces the chance of rollover (which would break sort
-  # order every time this occurs).
-  defp update_counter(ts, seed) do
-    :ets.update_counter(__MODULE__, ts, 1, {ts, seed})
-  end
-
-  # NOTE: I still want to benchmark different cutoffs and cleanup intervals.
-  defp cleanup(timestamp_ref, cutoff) do
-    timestamp = System.system_time(:millisecond)
-    previous_ts = :atomics.get(timestamp_ref, 1)
-
-    # If the last timestamp was over 10 seconds ago, then we don't bother to run the cleanup.
-    if timestamp - previous_ts < 10_000 do
-      # Cleanup all entries that are older than the cutoff.
-      :ets.select_delete(__MODULE__, [{{:"$1", :_}, [{:<, :"$1", timestamp - cutoff}], [true]}])
-    end
-  end
-
-  defp schedule_cleanup(interval_ms) do
-    Process.send_after(self(), :cleanup, interval_ms)
   end
 end
