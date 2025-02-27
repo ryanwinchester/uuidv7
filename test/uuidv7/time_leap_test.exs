2bb80eb2e71ce1a426382a28505d74c59a420385
Ryan Winchester <ryan@winchester.dev>
2025-02-26 20:51:41 -0400

    Update implementation to use new clock module

--- test/uuidv7/time_leap_test.exs
+++ test/uuidv7/time_leap_test.exs
@@ -4,28 +4,27 @@ defmodule UUIDv7.TimeLeapTest do
   alias UUIDv7.Clock
 
   test "next/1 protects against time leaping backwards" do
-    <<rand_a::17, _::7>> = :crypto.strong_rand_bytes(3)
-    {time1, _clock} = Clock.next(<<rand_a::17>>)
+    atomic_timer_ref = :persistent_term.get(Clock)
+
+    time1 = Clock.next_ascending()
 
     :timer.sleep(1)
 
-    <<rand_a::17, _::7>> = :crypto.strong_rand_bytes(3)
-    {time2, _clock} = Clock.next(<<rand_a::17>>)
+    time2 = Clock.next_ascending()
 
     :timer.sleep(1)
 
     future_timestamp =
       DateTime.utc_now()
       |> DateTime.add(1, :hour)
-      |> DateTime.to_unix(:millisecond)
+      |> DateTime.to_unix(:nanosecond)
 
-    :persistent_term.get(Clock) |> :atomics.put(1, future_timestamp)
+    :atomics.put(atomic_timer_ref, 1, future_timestamp)
 
-    <<rand_a::17, _::7>> = :crypto.strong_rand_bytes(3)
-    {time3, _clock} = Clock.next(<<rand_a::17>>)
+    time3 = Clock.next_ascending()
 
     assert time1 < time2
     assert time2 < time3
-    assert time3 == future_timestamp
+    assert time3 > future_timestamp
   end
 end
